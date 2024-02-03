5cb241165be9d4bda1da77f05e4a49fe6c77ec3b
Somuhack <somnathbarik516@gmail.com>
2024-02-03 11:06:28 +0530

    react

--- fragments/src/App.js
+++ fragments/src/App.js
@@ -4,15 +4,15 @@ import ErrorMessage from "./components/ErrorMessage";
 import FoodItem from "./components/FoodItem";
 function App() {
   //  let foodItem = []
-  let foodItem = ['Dal','Vegitable','Panir','Rice','salad','Milk']
+  let foodItem = ['Dal', 'Vegitable', 'Panir', 'Rice', 'salad', 'Milk']
   // let emptyMassage= foodItem.length===0 ? <h1>i am still Hungry</h1> : null
   return (
-   <>
-   <h1>Health Food</h1>
-    <ErrorMessage list={foodItem} />
-   <FoodItem item={foodItem} />
-   </>
+    <>
+      <h1>Health Food</h1>
+      <ErrorMessage list={foodItem} />
+      <FoodItem item={foodItem} />
+    </>
   );
-}
+};
 
 export default App;
